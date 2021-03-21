2f95849b1eed58a9e57560f5d5853cd5f8476a21
15Syncopation <fareezgaming@gmail.com>
2021-03-21 19:46:22 +0700

    Added unstable show function

--- generators/arithmetic.py
+++ generators/arithmetic.py
@@ -1,12 +1,18 @@
 import random
 import operator
 
+# Evaluate the questions
 
 def generate(total_questions, total_constants):
 	math_operators = ['+', '-', '*', '/']
+	
 	global questions
+	global answers
+	
 	questions = {}
 	question_number = 0
+	
+	answers = []
 	
 	generated_questions = 0
 	generated_constants = 0
@@ -31,5 +37,24 @@ def generate(total_questions, total_constants):
 				generated_questions += 1
 				generated_constants = 0
 	
+	if len(questions) != 0:
+		for answer in questions.values():
+			answers.append(eval(answer))
 	
-	return questions
+	return 0
+
+
+def show(show_questions, show_answers, return_both):
+	if show_questions == True:
+		return questions
+		
+	elif show_answers == True:
+		return answers
+	
+	elif return_both == True:
+		return questions, answers
+
+
+
+generate(4, 2)
+print(show(False, False, True))
